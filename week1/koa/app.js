8702650d8f070df3ff971c9532a865053d73c55f
Ilker <ikurtulan97@gmail.com>
2021-10-28 22:14:40 +0300

    Comments about Koa.js application

--- week1/koa/app.js
+++ week1/koa/app.js
@@ -1,8 +1,26 @@
 const Koa = require('koa');
 const app = new Koa();
 
-app.use(async ctx => {
-    ctx.body = 'Hello World';
+//Logger middleware
+app.use(async(ctx, next) => {
+    await next();
+    const rt = ctx.response.get('X-Response-Time');
+    console.log(`${ctx.method} ${ctx.url} - ${rt}`);
 });
 
-app.listen(3000);
+
+//x-response-time
+app.use(async(ctx, next) => {
+    const start = Date.now();
+    await next();
+    //Calculating the miliseconds here
+    const ms = Date.now() - start;
+    ctx.set('X-Response-Time', `${ms}ms`);
+});
+
+//Launching the Koa application
+app.use(async ctx => {
+    ctx.body = '<h1>Hello World !</h1>';
+});
+
+app.listen(5000);
