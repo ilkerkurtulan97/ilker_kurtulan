23b179e0f180ebc6f4dd5f99084dd6bd60e789b3
Ilker <ikurtulan97@gmail.com>
2021-11-04 09:15:53 +0300

    Db connection made. Ejs files edited. App routings are still in progress

--- week2/app.js
+++ week2/app.js
@@ -1,27 +1,54 @@
 const express = require('express');
-//const mongoose = require('mongoose');
-const path = require('path');
+const mongoose = require('mongoose');
 const ejs = require('ejs');
+const path = require('path');
+const Photo = require('./model/Photo');
+const env = require('dotenv');
 
-//Creating express application
+env.config();
+
+//Creating express app variable
 const app = express()
+
+//Creating our database connection
+mongoose.connect(process.env.MONGO, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+
+//Template engine
+app.set('view engine', 'ejs');
+
+//Middlewares
+//Lets express know our public folder
+app.use(express.static('public'));
+//Captures the url data
+app.use(express.urlencoded({ extended: true }));
+//Helps url data to be formatted in json
+app.use(express.json());
+
 
 const myLogger = (req, res, next) => {
     console.log("Middleware Log 1");
     next();
 }
 
-app.use(express.static('public'));
-app.use(myLogger);
+//app.use(myLogger);
 
 
 //Get request routing homepage
+/*
 app.get('/', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'temp/index.html'));
 });
+*/
 
-app.get("/", (req, res) => {
-    res.render('index');
+
+//Routing
+app.get("/", async(req, res) => {
+    const photos = await Photo.find({});
+    console.log(photos)
+    res.end();
 });
 
 app.get("/about", (req, res) => {
@@ -32,9 +59,22 @@ app.get("/add_post", (req, res) => {
     res.render('add_post');
 });
 
+app.post("/photos", async(req, res) => {
+    await Photo.create(req.body)
+    res.redirect('/')
+});
+
+
 
 //Launching the app and listening from port 3000
+const port = 3000;
+app.listen(port, () => {
+    console.log(`Sunucu ${port} portunda baslatildi...`);
+});
+
+/*
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log(`Sunucu ${port} portunda başlatıldı..`);
 });
+*/
