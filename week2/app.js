02d30e3c43da2f44a06024ab7fcb5cc468ec9785
Ilker <ikurtulan97@gmail.com>
2021-11-05 13:49:03 +0300

    first

--- week2/app.js
+++ week2/app.js
@@ -21,6 +21,10 @@ const port = process.env.PORT || 3000;
 mongoose.connect(process.env.MONGO, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
+}).then(() => {
+    console.log("Database connected !")
+}).catch((err) => {
+    console.log("Error occured: ", err);
 });
 
 //Template engine
